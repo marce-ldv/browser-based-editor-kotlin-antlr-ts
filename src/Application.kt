966c63058ced0bc0fdb42312b291309246fdb2ef
MarceloLudovino <ext-m.ludovino@rappi.com>
2020-06-05 12:16:59 -0300

    Serving files with ktor

--- src/Application.kt
+++ src/Application.kt
@@ -6,22 +6,35 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import java.io.File
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-    val server = embeddedServer(Netty, port = 8080) {
+    val port = if (args.isEmpty()) 8080 else args[0].toInt()
+    val server = embeddedServer(Netty, port = port) {
         routing {
-            get("/") {
-                call.respondText("Hello ANTLR!", ContentType.Text.Plain)
+            static("css") {
+                files("../src/main/css")
             }
-            get("/demo") {
-                call.respondText("HELLO ANTLR!")
+            static("js") {
+                files("../dist")
+            }
+            static("node_modules") {
+                files("../node_modules")
+            }
+            get("/") {
+                try {
+                    val text = File("../frontend/index.html").readText(Charsets.UTF_8)
+                    call.respondText(text, ContentType.Text.Html)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
             }
         }
     }
-    server.start(wait = true)
+    server.start(wait = false)
 }
 
