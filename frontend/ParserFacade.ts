77edc6c55a439de40bbac84e74a9f3d2182a630d
MarceloLudovino <ext-m.ludovino@rappi.com>
2020-06-05 12:10:26 -0300

    Configure webpack config

--- frontend/ParserFacade.ts
+++ frontend/ParserFacade.ts
@@ -1,6 +1,13 @@
 /// <reference path="../../../node_modules/monaco-editor/monaco.d.ts" />
-import { InputStream, Token } from 'antlr4'
 import { CalcLexer } from '../ANTLR/CalcLexer.js'
+import { CalcParser } from '../ANTLR/CalcParser.js'
+/// <reference path="./node_modules/monaco-editor/monaco.d.ts" />
+import { CommonTokenStream, InputStream, Token, error } from './node_modules/antlr4/index.js'
+class MyErrorListener extends error.ErrorListener {
+    syntaxError(recognizer, offendingSymbol, line, column, msg, e) {
+        console.log("ERROR " + msg);
+    }
+}
 function createLexer(input: String) {
     const chars = new InputStream(input);
     const lexer = new CalcLexer(chars);
@@ -9,4 +16,26 @@ function createLexer(input: String) {
 }
 export function getTokens(input: String) : Token[] {
     return createLexer(input).getAllTokens()
+}
+function createParser(input) {
+    const lexer = createLexer(input);
+    return createParserFromLexer(lexer);
+}
+function createParserFromLexer(lexer) {
+    const tokens = new CommonTokenStream(lexer);
+    return new CalcParser(tokens);
+}
+function parseTree(input) {
+    const parser = createParser(input);
+    return parser.compilationUnit();
+}
+export function parseTreeStr(input) {
+    const lexer = createLexer(input);
+    lexer.removeErrorListeners();
+    lexer.addErrorListener(new MyErrorListener());
+    const parser = createParserFromLexer(lexer);
+    parser.removeErrorListeners();
+    parser.addErrorListener(new MyErrorListener());
+    const tree = parser.compilationUnit();
+    return tree.toStringTree(parser.ruleNames);
 }
