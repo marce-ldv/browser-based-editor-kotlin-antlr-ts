23425c3bce2367845a640533230cb55115c059b0
MarceloLudovino <ext-m.ludovino@rappi.com>
2020-06-05 11:11:11 -0300

    Initializing project

---
+++ src/Application.kt
@@ -0,0 +1,27 @@
+package com.example
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    val server = embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello ANTLR!", ContentType.Text.Plain)
+            }
+            get("/demo") {
+                call.respondText("HELLO ANTLR!")
+            }
+        }
+    }
+    server.start(wait = true)
+}
+
